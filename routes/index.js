5c040b0670964d7d4b3c749fdf3270296f32481a
ewhitney0306 <113402902+ewhitney0306@users.noreply.github.com>
2024-09-27 13:16:44 -0700

    first commit Lab 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const routes = require('express').Router(); 
+//const router = express.Router();
 
 const myController = require ('../controllers');
 
-routes.get('/', myController.returnName);
+//routes.get('/', myController.returnName);
+
+routes.use('/contacts', require('./contact'));
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-app.use('/', require('./routes'));
+app 
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log (`Connected to DB and listening on ${port}`);
+    }
 });
